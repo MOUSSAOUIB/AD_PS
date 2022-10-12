fb71fab67d9f4ddce9f5ac7bde753025d8fb0cc7
Abdeslam Moussaoui <moussaouii.abdel@gmail.com>
2022-10-12 01:50:47 +0100

    Update 1_CREATE_USERS.ps1
    Adding comments

--- 1_CREATE_USERS.ps1
+++ 1_CREATE_USERS.ps1
@@ -2,14 +2,19 @@
 $PASSWORD_FOR_USERS   = "Password1"
 $USER_FIRST_LAST_LIST = Get-Content .\names.txt
 # ------------------------------------------------------ #
-
+ # create a password object for powershell #
 $password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+# create an organization unit called _USERS #
 New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
 
+
+# Craete users in AD, example of a user in USER_FIRST_LAST_LIST = "MOUSSAOUI ABDESLAM#
 foreach ($n in $USER_FIRST_LAST_LIST) {
-    $first = $n.Split(" ")[0].ToLower()
-    $last = $n.Split(" ")[1].ToLower()
-    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    $first = $n.Split(" ")[0].ToLower()     # -> moussaoui #
+    $last = $n.Split(" ")[1].ToLower()           # -> abdeslam #
+    $username = "$($first.Substring(0,1))$($last)".ToLower()  #mabdeslam#
+    
+    # print -> creating user: mabdeslam"
     Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
     
     New-AdUser -AccountPassword $password `
